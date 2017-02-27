cc71b8d199a3be389bef52b6ffb92515764be989
cutiko <cutiko@gmail.com>
2017-02-27 12:24:45 -0300

    ADD: validation to dont ask permissions if are granted

--- magicalpermissions/src/main/java/cl/cutiko/magicalpermissions/MagicalPermissions.java
+++ magicalpermissions/src/main/java/cl/cutiko/magicalpermissions/MagicalPermissions.java
@@ -2,9 +2,14 @@ package cl.cutiko.magicalpermissions;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by cutiko on 25-02-17.
@@ -45,10 +50,24 @@ public class MagicalPermissions {
 
     @SuppressLint("NewApi")
     private void requestPermissions() {
-        if (activity != null) {
-            activity.requestPermissions(permissions, RC_PERMISSIONS_ACTIVITY);
-        } else {
-            fragment.requestPermissions(permissions, RC_PERMISSIONS_FRAGMENT);
+        final Context context = (activity != null) ? activity : fragment.getContext();
+
+        List<String> permissionList = new ArrayList<>();
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                permissionList.add(permission);
+            }
+        }
+
+        //Then only if there are permissions to be requested, only request those
+        if (permissionList.size() > 0) {
+            //If there are permissions to be requested then we can redefine the original array with the ones we have to take care
+            permissions = permissionList.toArray(new String[permissionList.size()]);
+            if (activity != null) {
+                activity.requestPermissions(permissions, RC_PERMISSIONS_ACTIVITY);
+            } else {
+                fragment.requestPermissions(permissions, RC_PERMISSIONS_FRAGMENT);
+            }
         }
     }
 
